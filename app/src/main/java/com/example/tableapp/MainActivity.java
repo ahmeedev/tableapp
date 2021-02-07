ecca8300055a1782927cf7311f0accf9789eb2d1
Ahmad Tariq <ahmadbintariq4u@gmail.com>
2021-02-07 21:09:43 +0500

    Added Table App

---
+++ app/src/main/java/com/example/tableapp/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.tableapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.SeekBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        SeekBar bar=findViewById(R.id.seekBar);
+
+        bar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                System.out.println("hello");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+    }
+}
